8bb6922ec55e9688f1366acdc5e2f48baea3d732
more-devs <moresitoes@gmail.com>
2025-06-11 02:28:59 -0400

    comit3frontend

---
+++ src/components/UsuarioList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h2>Usuarios</h2>
+    <ul>
+      <li v-for="usuario in usuarios" :key="usuario.id">
+        {{ usuario.nombre }} - {{ usuario.edad }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import api from '../services/api';
+
+export default {
+  data() {
+    return {
+      usuarios: []
+    };
+  },
+  async created() {
+    try {
+      const res = await api.get('/usuarios');
+      this.usuarios = res.data;
+    } catch (error) {
+      console.error('Error al cargar usuarios:', error);
+    }
+  }
+};
+</script>
